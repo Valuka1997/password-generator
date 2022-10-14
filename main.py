fc203e3178c1ca3655a03ef49708716b641157a0
Valuka gimhan <valukagimhan@gmail.com>
2022-10-14 12:26:17 +0530

    v2

--- main.py
+++ main.py
@@ -2,31 +2,10 @@ from tkinter import *
 import string
 import random
 
+
 def pw_gen():
-    print("abcabc")
-    selection=''
-    # if letters.get() == 1 | numbers.get() == 1 | special.get() == 1:
-    #     selection = string.ascii_letters + string.digits + string.punctuation
-    #
-    # elif letters.get() == 1 | numbers.get() == 0 | special.get() == 1:
-    #     selection = string.ascii_letters + string.punctuation
-    #
-    # elif letters.get() == 1 | numbers.get() == 1 | special.get() == 0:
-    #     selection = string.ascii_letters + string.digits
-    #
-    # elif letters.get() == 1 | numbers.get() == 0 | special.get() == 0:
-    #     selection = string.ascii_letters
-    #
-    # elif letters.get() == 0 | numbers.get() == 1 | special.get() == 1:
-    #     selection =  string.digits + string.punctuation
-    #
-    # elif letters.get() == 0 | numbers.get() == 1 | special.get() == 0:
-    #     selection =  string.digits
-    #
-    # elif letters.get() == 0 | numbers.get() == 0 | special.get() == 1:
-    #     selection =  string.punctuation
-    #
-    if letters.get() == 1 :
+    selection = ''
+    if letters.get() == 1:
         selection = selection + string.ascii_letters
 
     if numbers.get() == 1:
@@ -40,24 +19,33 @@ def pw_gen():
     pwd = "".join([random.choice(selection) for _ in range(pwdlen)])
     pswdbox.insert(0, pwd)
 
+
 def chckd():
-    if letters.get() == 1 :
-        generate_password_button['state'] = NORMAL
-        generate_password_button.configure(text='Generate Password!')
+    if len(pw_length.get()) != 0:
 
-    elif numbers.get() == 1:
-        generate_password_button['state'] = NORMAL
-        generate_password_button.configure(text='Generate Password!')
+        if letters.get() == 1:
+            generate_password_button['state'] = NORMAL
+            generate_password_button.configure(text='Generate Password!')
 
-    elif special.get() == 1 :
-        generate_password_button['state'] = NORMAL
-        generate_password_button.configure(text='Generate Password!')
+        elif numbers.get() == 1:
+            generate_password_button['state'] = NORMAL
+            generate_password_button.configure(text='Generate Password!')
+
+        elif special.get() == 1:
+            generate_password_button['state'] = NORMAL
+            generate_password_button.configure(text='Generate Password!')
+
+        else:
+            generate_password_button['state'] = DISABLED
+            generate_password_button.configure(
+                text='Check at least one option!')
 
     else:
         generate_password_button['state'] = DISABLED
         generate_password_button.configure(text='Check at least one option!')
 
-#user inerface
+# user inerface
+
 
 ui = Tk()
 ui.title("Strong Password Generator")
@@ -82,21 +70,26 @@ pw_length.pack()
 letters = IntVar()
 numbers = IntVar()
 special = IntVar()
-C1 = Checkbutton(ui, text = "Letters", variable = letters, onvalue = 1, offvalue = 0, height=5, width = 20, command=chckd)
-C2 = Checkbutton(ui, text = "Numbers", variable = numbers, onvalue = 1, offvalue = 0, height=5, width = 20, command=chckd)
-C3 = Checkbutton(ui, text = "Special Characters", variable = special, onvalue = 1, offvalue = 0, height=5, width = 20, command=chckd)
+C1 = Checkbutton(ui, text="Letters", variable=letters, onvalue=1,
+                 offvalue=0, height=5, width=20, command=chckd)
+C2 = Checkbutton(ui, text="Numbers", variable=numbers, onvalue=1,
+                 offvalue=0, height=5, width=20, command=chckd)
+C3 = Checkbutton(ui, text="Special Characters", variable=special,
+                 onvalue=1, offvalue=0, height=5, width=20, command=chckd)
 #C1.grid(row=1, column=2)
-#C2.grid(row=2,column=2)
+# C2.grid(row=2,column=2)
 #C3.grid(row=3, column=2)
 C1.pack()
 C2.pack()
 C3.pack()
 
-generate_password_button = Button(ui, text="Check at least one option!",state=DISABLED, bg="#fb743e", height=4, width=55, command=pw_gen)
+generate_password_button = Button(ui, text="Check at least one option!",
+                                  state=DISABLED, bg="#fb743e", height=4, width=55, command=pw_gen)
 #generate_password_button.grid(row=4, column=0, columnspan=3)
 generate_password_button.pack()
 
-pswdbox = Entry(bg="#383e56",font=("Arial", 15, "bold"), width=40)
+
+pswdbox = Entry(bg="#383e56", font=("Arial", 15, "bold"), width=40)
 #pswdbox.grid(row=3, column=0, columnspan=3)
 pswdbox.pack()
 
